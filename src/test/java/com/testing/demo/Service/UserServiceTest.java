6849b31e79494698111bbfdc2bfdf666da8b1b56
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-05-26 18:35:54 +0530

    Testing File created Datasource need to be added.

--- src/test/java/com/testing/demo/Service/UserServiceTest.java
+++ src/test/java/com/testing/demo/Service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.testing.demo.Service;
 
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
 import com.testing.demo.Model.Request.Users;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +15,11 @@ class UserServiceTest {
     private Users users;
     private UserService userService;
 
+    //Preparing the testing environment.
+    ServerAddress serverAddress = new ServerAddress("127.0.0.1", 27017);
+//    MongoClient mongoClient = new MongoClient(serverAddress);
+//    MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, "db");
+
     @BeforeEach
     void setUp() {
 
@@ -20,6 +27,7 @@ class UserServiceTest {
 
     @AfterEach
     void tearDown() {
+
     }
 
     @Test
@@ -28,17 +36,26 @@ class UserServiceTest {
 
     @Test
     void getAllUsers() {
+        List<String> users = List.of("firstName","lastName","userName", "pwd", "type");
+        Assertions.assertEquals("firstName", users.get(0));
+        Assertions.assertEquals("lastName", users.get(1));
+        Assertions.assertEquals("userName", users.get(2));
+        Assertions.assertEquals("pwd", users.get(3));
+        Assertions.assertEquals("type", users.get(4));
     }
 
     @Test
     void getUserById() {
+
     }
 
     @Test
     void generateToken() {
+
     }
 
     @Test
     void validateUser() {
+
     }
 }
