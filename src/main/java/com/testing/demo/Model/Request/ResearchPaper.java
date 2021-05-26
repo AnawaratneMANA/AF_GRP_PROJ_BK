2080611f1d37947e74fc8a227109874e14a8a1db
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-05-26 15:24:55 +0530

    Individual Updates by Menaka

--- src/main/java/com/testing/demo/Model/Request/ResearchPaper.java
+++ src/main/java/com/testing/demo/Model/Request/ResearchPaper.java
@@ -1,4 +1,13 @@
 package com.testing.demo.Model.Request;
 
+import org.springframework.data.annotation.Id;
+
 public class ResearchPaper {
+
+    @Id
+    private String id;
+    private String fileName;
+    private String path;
+    private String publisherId;
+    private String category;
 }
