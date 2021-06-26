05703185b1883507feaf25981f15b4b807f88b9d
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-26 17:46:30 +0530

    DownloadCategory Controller class created with insert an delete methods.

--- src/main/java/com/testing/demo/Service/DownloadServiceImpl.java
+++ src/main/java/com/testing/demo/Service/DownloadServiceImpl.java
@@ -5,9 +5,10 @@ import com.testing.demo.Model.Request.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
-
+@Service
 public class DownloadServiceImpl implements DownloadService{
     @Autowired
     MongoTemplate mongoTemplate;
@@ -28,6 +29,5 @@ public class DownloadServiceImpl implements DownloadService{
     public String deleteDownloadCategory(String type) {
         mongoTemplate.remove(new Query(where("type").is(type)), DownloadCategory.class);
         return "User id is " + type;
-
     }
 }
