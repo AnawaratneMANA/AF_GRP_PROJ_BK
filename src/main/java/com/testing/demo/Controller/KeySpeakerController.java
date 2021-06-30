5b0ddd8188d7d2d39cebde6783df584e13f5e165
Taneesha <taneesha.chandrasena@acentura.com>
2021-06-30 13:58:15 +0530

    KeySpeaker controller get all key speaker method done

--- src/main/java/com/testing/demo/Controller/KeySpeakerController.java
+++ src/main/java/com/testing/demo/Controller/KeySpeakerController.java
@@ -1,5 +1,6 @@
 package com.testing.demo.Controller;
 
+import com.testing.demo.Model.Request.Feedback;
 import com.testing.demo.Model.Request.KeySpeakers;
 import com.testing.demo.Service.KeySpeakerService.KeySpeakerService;
 import com.testing.demo.util.JwtUtil;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -27,4 +30,20 @@ public class KeySpeakerController {
         KeySpeakers addedKeySpeaker = keySpeakerService.createKeySpeaker(keySpeakers);
         return new ResponseEntity<>(keySpeakers, HttpStatus.OK);
     }
+
+    @GetMapping("/keyspeakers")
+    public ResponseEntity<?> getAllKeySpeaker(){
+        List<KeySpeakers> keySpeakers = keySpeakerService.getAllKeySpeaker();
+        if(keySpeakers.size() > 0){
+            return new ResponseEntity<>(keySpeakers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("No KeySpeakers", HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
+
+
 }
