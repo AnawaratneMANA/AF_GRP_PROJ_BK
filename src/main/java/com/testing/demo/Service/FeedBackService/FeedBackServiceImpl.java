2e850ae79591e8baf88083cae77128939c63bc20
Taneesha <taneesha.chandrasena@acentura.com>
2021-06-30 11:19:33 +0530

    createFeedback method implementation done

--- src/main/java/com/testing/demo/Service/FeedBackService/FeedBackServiceImpl.java
+++ src/main/java/com/testing/demo/Service/FeedBackService/FeedBackServiceImpl.java
@@ -1,5 +1,58 @@
 package com.testing.demo.Service.FeedBackService;
 
-public class FeedBackServiceImpl {
+import com.testing.demo.Model.Request.Events;
+import com.testing.demo.Model.Request.Feedback;
+import com.testing.demo.Repository.BarChartMongoRepo;
+import com.testing.demo.Repository.Eventrepository;
+import com.testing.demo.Service.EventService.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class FeedBackServiceImpl implements FeedBackService{
+    @Autowired
+    Eventrepository eventrepository;
+
+    @Autowired
+    EventService eventService;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Autowired
+    BarChartMongoRepo barChartMongoRepo;
+
+    @Override
+    public Feedback createFeedback(Feedback feedback) {
+        Feedback newFeedBack = new Feedback();
+        newFeedBack.setName(feedback.getName());
+        newFeedBack.setFeedback(feedback.getFeedback());
+        newFeedBack.setRating(feedback.getRating());
+
+        mongoTemplate.insert(newFeedBack);
+        return newFeedBack;
+    }
+
+    @Override
+    public List<Events> getAllFeedbacks() {
+        return null;
+    }
+
+    @Override
+    public Feedback getFeedBackById(String id) {
+        return null;
+    }
+
+    @Override
+    public String deleteFeedback(String id) {
+        return null;
+    }
+
+    @Override
+    public Feedback getFeedbackByName(String name) {
+        return null;
+    }
 }
